f4d1252d2f5f3d96400248995db47ee1d8317eaa
jby9725 <jby9725@gmail.com>
2024-08-09 10:38:27 +0900

    Controller count 변수 컨트롤

--- src/main/java/com/example/demo/controller/UsrHomeController.java
+++ src/main/java/com/example/demo/controller/UsrHomeController.java
@@ -6,9 +6,49 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class UsrHomeController {
+	
+	private int count;
+	
+	public UsrHomeController() {
+		count = 0;
+	}
+	
 	@RequestMapping("/usr/home/main")
 	@ResponseBody
 	public String showMain() {
 		return "Hi! Hello~!";
 	}
+	
+	@RequestMapping("/usr/home/main2")
+	@ResponseBody
+	public String showMain2() {
+		return "잘가세요!";
+	}
+	
+	@RequestMapping("/usr/home/main3")
+	@ResponseBody
+	public int showMain3() {
+		return 1+2;
+	}
+	
+	@RequestMapping("/usr/home/getCount")
+	@ResponseBody
+	public int showMain4() {
+		return count++;
+	}
+
+	@RequestMapping("/usr/home/setCountZero")
+	@ResponseBody
+	public String setCountZero() {
+		count = 0;
+		return "count 0 초기화";
+	}
+
+	@RequestMapping("/usr/home/setCountValue")
+	@ResponseBody
+	public String setCountValue(int value) {
+		count = value;
+		return "count "+ value +"(으)로 초기화";
+	}
+	
 }

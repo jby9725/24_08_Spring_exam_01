d410d40c254768f9526f770e39b7d3ce03b84c62
jby9725 <jby9725@gmail.com>
2024-08-28 11:04:56 +0900

    로그인 한 회원에 따라서 댓글 수정, 삭제 버튼 표시

--- src/main/java/com/example/demo/MyWebMVCConfigurer.java
+++ src/main/java/com/example/demo/MyWebMVCConfigurer.java
@@ -57,6 +57,8 @@ public class MyWebMVCConfigurer implements WebMvcConfigurer {
 		ir.addPathPatterns("/usr/reactionPoint/doGoodReaction");
 		ir.addPathPatterns("/usr/reactionPoint/doBadReaction");
 
+		ir.addPathPatterns("/usr/reply/doWrite");
+		
 		ir = registry.addInterceptor(needLogoutInterceptor);
 		ir.addPathPatterns("/usr/member/login");
 		ir.addPathPatterns("/usr/member/doLogin");

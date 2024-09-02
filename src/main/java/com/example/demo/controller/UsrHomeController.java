2350f8e4f0fa4ae8a7006df480df8d94f4eac906
jby9725 <jby9725@gmail.com>
2024-09-02 15:55:59 +0900

    melon Top100 크롤링(Selenium)

--- src/main/java/com/example/demo/controller/UsrHomeController.java
+++ src/main/java/com/example/demo/controller/UsrHomeController.java
@@ -3,6 +3,8 @@ package com.example.demo.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.example.demo.util.crawlTest;
+
 @Controller
 public class UsrHomeController {
 
@@ -10,10 +12,17 @@ public class UsrHomeController {
 	public String showRoot() {
 		return "redirect:/usr/home/main";
 	}
-	
+
 	@RequestMapping("/usr/home/main")
 	public String showMain() {
 		return "/usr/home/main";
 	}
 
+	@RequestMapping("/usr/crawl")
+	public String doCrawl() {
+
+		crawlTest.crawl();
+
+		return "redirect:/usr/home/main";
+	}
 }
